b901ca50c2235d05716eb6a6971da03e10f93531
Avraham Micael <avrahammicael@protonmail.com>
2023-02-28 10:17:26 -0300

    added 'member' & 'leader' to cases

--- utils/getRoleAndUpperRoles.js
+++ utils/getRoleAndUpperRoles.js
@@ -2,12 +2,11 @@ module.exports = role => {
   const roles = ['leader'];
   switch(role)
   {
+    case 'member': roles.push('member');
     case 'elder':
-    case 'admin':
-      roles.push('admin'); 
-    case 'coLeader':
-      roles.push('coLeader');
-      break;
+    case 'admin': roles.push('admin'); 
+    case 'coLeader': roles.push('coLeader');
+    case 'leader': break;
     default:
       throw new Error('Invalid role');
   }
